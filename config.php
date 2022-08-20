92676b9e16956bdf328befac91db1ac1599b70f1
EnyanX <109812678+EnyanX@users.noreply.github.com>
2022-08-21 04:49:21 +0800

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	session_start();
-
-	define('DB_HOST', '303.itpwebdev.com');
-	define('DB_USER', 'enyanxia_db_user');
-	define('DB_PASS', '******');
-	define('DB_NAME', 'enyanxia_final_project_db');
-?>
